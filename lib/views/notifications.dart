71de2cc3d22fa6a4eb30f97adbb069751272f79a
JurojinKun <ccommunay@gmail.com>
2022-06-19 20:00:31 +0200

    config notifications android

---
+++ lib/views/notifications.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Notifications extends StatefulWidget {
+  const Notifications({Key? key}) : super(key: key);
+
+  @override
+  State<Notifications> createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text("Notifications"),
+    ));
+  }
+}
