f6f27f0590f12c2b6815bb767bc503605997aa3c
Communay Clément <ccommunay@gmail.com>
2022-12-28 20:43:29 +0100

    Commit 28/12/22 . optim ui responsive welcome screen

--- lib/views/nonAuth/welcome.dart
+++ lib/views/nonAuth/welcome.dart
@@ -51,141 +51,155 @@ class WelcomeState extends ConsumerState<Welcome> {
                 : const SystemUiOverlayStyle(
                     statusBarColor: Colors.transparent,
                     statusBarIconBrightness: Brightness.light),
-        child: Center(
-          child: Column(
-            children: [
-              Expanded(
-                child: Column(
+        child: SafeArea(
+          left: false,
+          right: false,
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(child: _assetTitle()),
+                Expanded(
+                    child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset("assets/images/ic_app.png", height: 125, width: 125),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    Text("My youkounkoun",
-                        style: textStyleCustomBold(
-                            Theme.of(context).brightness == Brightness.light
-                                ? cBlack
-                                : cWhite,
-                            33),
-                        textAlign: TextAlign.center,
-                        textScaleFactor: 1.0),
+                    Expanded(child: _btnsUser()),
+                    Expanded(child: _infosApp())
                   ],
-                ),
-              ),
-              Expanded(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _assetTitle() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/ic_app.png", height: 125, width: 125),
+        const SizedBox(
+          height: 25.0,
+        ),
+        Text("My youkounkoun",
+            style: textStyleCustomBold(
+                Theme.of(context).brightness == Brightness.light
+                    ? cBlack
+                    : cWhite,
+                33),
+            textAlign: TextAlign.center,
+            textScaleFactor: 1.0),
+      ],
+    );
+  }
+
+  Widget _btnsUser() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 50.0,
+          width: MediaQuery.of(context).size.width - 55,
+          child: ElevatedButton(
+              onPressed: () => navNonAuthKey.currentState!.pushNamed(login),
+              child: Text(
+                  AppLocalization.of(context)
+                      .translate("welcome_screen", "login"),
+                  style: textStyleCustomMedium(
+                      Theme.of(context).brightness == Brightness.light
+                          ? cBlack
+                          : cWhite,
+                      20),
+                  textScaleFactor: 1.0)),
+        ),
+        const SizedBox(
+          height: 55.0,
+        ),
+        SizedBox(
+          height: 50.0,
+          width: MediaQuery.of(context).size.width - 55,
+          child: ElevatedButton(
+              onPressed: () => navNonAuthKey.currentState!.pushNamed(register),
+              child: Text(
+                  AppLocalization.of(context)
+                      .translate("welcome_screen", "register"),
+                  style: textStyleCustomMedium(
+                      Theme.of(context).brightness == Brightness.light
+                          ? cBlack
+                          : cWhite,
+                      20),
+                  textScaleFactor: 1.0)),
+        ),
+      ],
+    );
+  }
+
+  Widget _infosApp() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RichText(
+            textAlign: TextAlign.center,
+            textScaleFactor: 1.0,
+            text: TextSpan(
+                text: AppLocalization.of(context)
+                    .translate("welcome_screen", "consult"),
+                style: textStyleCustomMedium(
+                    Theme.of(context).brightness == Brightness.light
+                        ? cBlack
+                        : cWhite,
+                    12),
                 children: [
-                  SizedBox(
-                    height: 50.0,
-                    width: MediaQuery.of(context).size.width - 55,
-                    child: ElevatedButton(
-                        onPressed: () =>
-                            navNonAuthKey.currentState!.pushNamed(login),
-                        child: Text(
-                            AppLocalization.of(context)
-                                .translate("welcome_screen", "login"),
-                            style: textStyleCustomMedium(
-                                Theme.of(context).brightness == Brightness.light
-                                    ? cBlack
-                                    : cWhite,
-                                20),
-                            textScaleFactor: 1.0)),
-                  ),
-                  const SizedBox(
-                    height: 55.0,
-                  ),
-                  SizedBox(
-                    height: 50.0,
-                    width: MediaQuery.of(context).size.width - 55,
-                    child: ElevatedButton(
-                        onPressed: () =>
-                            navNonAuthKey.currentState!.pushNamed(register),
-                        child: Text(
-                            AppLocalization.of(context)
-                                .translate("welcome_screen", "register"),
-                            style: textStyleCustomMedium(
-                                Theme.of(context).brightness == Brightness.light
-                                    ? cBlack
-                                    : cWhite,
-                                20),
-                            textScaleFactor: 1.0)),
-                  )
-                ],
-              )),
-              Container(
-                height: 40.0,
-                alignment: Alignment.center,
-                child: RichText(
-                    textAlign: TextAlign.center,
-                    textScaleFactor: 1.0,
-                    text: TextSpan(
-                        text: AppLocalization.of(context)
-                            .translate("welcome_screen", "consult"),
-                        style: textStyleCustomMedium(
-                            Theme.of(context).brightness == Brightness.light
-                                ? cBlack
-                                : cWhite,
-                            12),
-                        children: [
-                          TextSpan(
-                              text: AppLocalization.of(context)
-                                  .translate("welcome_screen", "cgu"),
-                              style: textStyleCustomMedium(cBlue, 12),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  //change url google to url cgu
-                                  Helpers.launchMyUrl("https://www.google.fr/");
-                                }),
-                          TextSpan(
-                            text: AppLocalization.of(context)
-                                .translate("welcome_screen", "and"),
-                            style: textStyleCustomMedium(
-                                Theme.of(context).brightness == Brightness.light
-                                    ? cBlack
-                                    : cWhite,
-                                12),
-                          ),
-                          TextSpan(
-                              text: AppLocalization.of(context).translate(
-                                  "welcome_screen", "privacy_policy"),
-                              style: textStyleCustomMedium(cBlue, 12),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  //change url google to url privacy policy
-                                  Helpers.launchMyUrl("https://www.google.fr/");
-                                }),
-                          TextSpan(
-                            text: AppLocalization.of(context)
-                                .translate("welcome_screen", "youkounkoun"),
-                            style: textStyleCustomMedium(
-                                Theme.of(context).brightness == Brightness.light
-                                    ? cBlack
-                                    : cWhite,
-                                12),
-                          )
-                        ])),
-              ),
-              Container(
-                height: 60.0,
-                alignment: Alignment.center,
-                child: Text(
-                    AppLocalization.of(context)
-                            .translate("welcome_screen", "version") +
-                        version,
-                    textAlign: TextAlign.center,
+                  TextSpan(
+                      text: AppLocalization.of(context)
+                          .translate("welcome_screen", "cgu"),
+                      style: textStyleCustomMedium(cBlue, 12),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          //change url google to url cgu
+                          Helpers.launchMyUrl("https://www.google.fr/");
+                        }),
+                  TextSpan(
+                    text: AppLocalization.of(context)
+                        .translate("welcome_screen", "and"),
                     style: textStyleCustomMedium(
                         Theme.of(context).brightness == Brightness.light
                             ? cBlack
                             : cWhite,
-                        11),
-                    textScaleFactor: 1.0),
-              )
-            ],
-          ),
-        ),
-      ),
+                        12),
+                  ),
+                  TextSpan(
+                      text: AppLocalization.of(context)
+                          .translate("welcome_screen", "privacy_policy"),
+                      style: textStyleCustomMedium(cBlue, 12),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          //change url google to url privacy policy
+                          Helpers.launchMyUrl("https://www.google.fr/");
+                        }),
+                  TextSpan(
+                    text: AppLocalization.of(context)
+                        .translate("welcome_screen", "youkounkoun"),
+                    style: textStyleCustomMedium(
+                        Theme.of(context).brightness == Brightness.light
+                            ? cBlack
+                            : cWhite,
+                        12),
+                  )
+                ])),
+        const SizedBox(height: 20.0),
+        Text(
+            AppLocalization.of(context).translate("welcome_screen", "version") +
+                version,
+            textAlign: TextAlign.center,
+            style: textStyleCustomMedium(
+                Theme.of(context).brightness == Brightness.light
+                    ? cBlack
+                    : cWhite,
+                11),
+            textScaleFactor: 1.0)
+      ],
     );
   }
 }
