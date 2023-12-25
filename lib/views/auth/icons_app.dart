ac7045f536ae9f7067e4f335c023102d3e3be255
0ruj <ccommunay@gmail.com>
2023-12-25 23:48:52 +0100

    Commit 25/12/23 - logic/configs android dynamic icon app change

--- lib/views/auth/icons_app.dart
+++ lib/views/auth/icons_app.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_android_dynamic_icon/android_dynamic_icon.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dynamic_icon/flutter_dynamic_icon.dart';
+import 'package:myyoukounkoun/components/message_user_custom.dart';
+
 import 'package:myyoukounkoun/constantes/constantes.dart';
 import 'package:myyoukounkoun/helpers/helpers.dart';
+import 'package:myyoukounkoun/libraries/sync_shared_prefs_lib.dart';
 import 'package:myyoukounkoun/models/icons_app_model.dart';
 import 'package:myyoukounkoun/translations/app_localizations.dart';
 
@@ -17,6 +23,8 @@ class IconsApp extends ConsumerStatefulWidget {
 class IconsAppState extends ConsumerState<IconsApp> {
   AppBar appBar = AppBar();
 
+  final _androidDynamicIconPlugin = AndroidDynamicIcon();
+
   List<IconsAppModel> icons = [
     IconsAppModel(asset: "assets/icons_app/ic_default.png", name: "Default"),
     IconsAppModel(
@@ -26,7 +34,88 @@ class IconsAppState extends ConsumerState<IconsApp> {
     IconsAppModel(asset: "assets/icons_app/ic_space.png", name: "Space"),
   ];
 
-  String choiceIcon = "Default";
+  String choiceIcon =
+      SyncSharedPrefsLib().prefs!.getString("iconApp") ?? "Default";
+
+  bool validModif = false;
+
+  Future<void> changeDynamicallyIconApp(String iconName) async {
+    try {
+      switch (iconName) {
+        case "Default":
+          if (Platform.isAndroid) {
+            await _androidDynamicIconPlugin.changeIcon(
+                bundleId: "com.example.myyoukounkoun",
+                isNewIcon: false,
+                iconName: "",
+                iconNames: ["JustBlack", "JustBlue", "Space"]);
+          } else if (Platform.isIOS &&
+              await FlutterDynamicIcon.supportsAlternateIcons) {
+            await FlutterDynamicIcon.setAlternateIconName(null);
+          }
+          break;
+        case "Just black":
+          if (Platform.isAndroid) {
+            await _androidDynamicIconPlugin.changeIcon(
+                bundleId: "com.example.myyoukounkoun",
+                isNewIcon: true,
+                iconName: "JustBlack",
+                iconNames: ["JustBlack", "JustBlue", "Space"]);
+          } else if (Platform.isIOS &&
+              await FlutterDynamicIcon.supportsAlternateIcons) {
+            await FlutterDynamicIcon.setAlternateIconName("just_black");
+          }
+          break;
+        case "Just blue":
+          if (Platform.isAndroid) {
+            await _androidDynamicIconPlugin.changeIcon(
+                bundleId: "com.example.myyoukounkoun",
+                isNewIcon: true,
+                iconName: "JustBlue",
+                iconNames: ["JustBlack", "JustBlue", "Space"]);
+          } else if (Platform.isIOS &&
+              await FlutterDynamicIcon.supportsAlternateIcons) {
+            await FlutterDynamicIcon.setAlternateIconName("just_blue");
+          }
+          break;
+        case "Space":
+          if (Platform.isAndroid) {
+            await _androidDynamicIconPlugin.changeIcon(
+                bundleId: "com.example.myyoukounkoun",
+                isNewIcon: true,
+                iconName: "Space",
+                iconNames: ["JustBlack", "JustBlue", "Space"]);
+          } else if (Platform.isIOS &&
+              await FlutterDynamicIcon.supportsAlternateIcons) {
+            await FlutterDynamicIcon.setAlternateIconName("space");
+          }
+          break;
+        default:
+          if (Platform.isAndroid) {
+            await _androidDynamicIconPlugin.changeIcon(
+                bundleId: "com.example.myyoukounkoun",
+                isNewIcon: false,
+                iconName: "",
+                iconNames: ["JustBlack", "JustBlue", "Space"]);
+          } else if (Platform.isIOS &&
+              await FlutterDynamicIcon.supportsAlternateIcons) {
+            await FlutterDynamicIcon.setAlternateIconName(null);
+          }
+      }
+      await SyncSharedPrefsLib().prefs!.setString('iconApp', choiceIcon);
+      if (mounted) {
+        messageUser(
+            context,
+            AppLocalization.of(context)
+                .translate("icons_app_screen", "success_message"));
+      }
+    } catch (e) {
+      if (mounted) {
+        messageUser(context,
+            AppLocalization.of(context).translate("general", "message_error"));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,30 +153,40 @@ class IconsAppState extends ConsumerState<IconsApp> {
             ),
           ),
         ),
-        body: SizedBox.expand(
-            child: ListView.builder(
-          padding: EdgeInsets.fromLTRB(
-              20.0,
-              MediaQuery.of(context).padding.top +
-                  appBar.preferredSize.height +
+        body: Stack(
+          children: [
+            SizedBox.expand(
+                child: ListView.builder(
+              padding: EdgeInsets.fromLTRB(
                   20.0,
-              20.0,
-              MediaQuery.of(context).padding.bottom + 20.0),
-          physics: const AlwaysScrollableScrollPhysics(
-              parent: BouncingScrollPhysics()),
-          itemCount: icons.length,
-          itemBuilder: (context, index) {
-            IconsAppModel icon = icons[index];
-            return iconItem(icon);
-          },
-        )));
+                  MediaQuery.of(context).padding.top +
+                      appBar.preferredSize.height +
+                      20.0,
+                  20.0,
+                  MediaQuery.of(context).padding.bottom + 20.0),
+              physics: const AlwaysScrollableScrollPhysics(
+                  parent: BouncingScrollPhysics()),
+              itemCount: icons.length,
+              itemBuilder: (context, index) {
+                IconsAppModel icon = icons[index];
+                return _iconItem(icon);
+              },
+            )),
+            if ((SyncSharedPrefsLib().prefs!.getString("iconApp") != null &&
+                    choiceIcon !=
+                        SyncSharedPrefsLib().prefs!.getString("iconApp")) ||
+                (SyncSharedPrefsLib().prefs!.getString("iconApp") == null &&
+                    choiceIcon != "Default"))
+              _saveNewIconApp()
+          ],
+        ));
   }
 
-  Widget iconItem(IconsAppModel icon) {
+  Widget _iconItem(IconsAppModel icon) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: ListTile(
-        onTap: () {
+        onTap: () async {
           setState(() {
             choiceIcon = icon.name;
           });
@@ -116,6 +215,89 @@ class IconsAppState extends ConsumerState<IconsApp> {
                 choiceIcon = icon.name;
               });
             }),
+      ),
+    );
+  }
+
+  Widget _saveNewIconApp() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        height: 100.0,
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SizedBox(
+                    height: 50.0,
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          if (!validModif) {
+                            setState(() {
+                              validModif = true;
+                            });
+                            await changeDynamicallyIconApp(choiceIcon);
+                            setState(() {
+                              validModif = false;
+                            });
+                          }
+                        },
+                        child: validModif
+                            ? const SizedBox(
+                                height: 15.0,
+                                width: 15.0,
+                                child: Center(
+                                  child: CircularProgressIndicator(
+                                    color: cWhite,
+                                    strokeWidth: 1.0,
+                                  ),
+                                ),
+                              )
+                            : Text(
+                                AppLocalization.of(context).translate(
+                                    "icons_app_screen", "save_new_icon_app"),
+                                style: textStyleCustomMedium(
+                                    Theme.of(context).brightness ==
+                                            Brightness.light
+                                        ? cBlack
+                                        : cWhite,
+                                    20),
+                                textAlign: TextAlign.center,
+                                textScaler: const TextScaler.linear(1.0)))),
+              ),
+              const SizedBox(width: 15.0),
+              Expanded(
+                child: SizedBox(
+                    height: 50.0,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: cRed,
+                          foregroundColor: Colors.white,
+                          shadowColor: Colors.transparent,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            choiceIcon = SyncSharedPrefsLib()
+                                    .prefs!
+                                    .getString("iconApp") ??
+                                "Default";
+                          });
+                        },
+                        child: Text(
+                            AppLocalization.of(context)
+                                .translate("general", "btn_cancel"),
+                            style: textStyleCustomMedium(
+                                Helpers.uiApp(context), 20),
+                            textScaler: const TextScaler.linear(1.0)))),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
